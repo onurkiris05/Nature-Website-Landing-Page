5f753090bcb2db926938f1328c9534c2a17a69b7
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-02-06 19:16:21 +0300

    [Added] Background

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const navMenuButtons = document.querySelectorAll(".toggle-nav");
 const colorButtons = document.querySelectorAll(".toggle-color");
 const nav = document.querySelector(".navbar");
+const bgVideo = document.querySelector(".bg-video");
 
 // Toggle Nav
 function toggleNav() {
@@ -19,7 +20,12 @@ function toggleColorMode() {
   colorButtons.forEach((button) => {
     button.classList.toggle("active");
   });
+
   document.body.classList.toggle("dark-mode");
+  const isDarkMode = document.body.classList.contains("dark-mode");
+  bgVideo.src = isDarkMode
+    ? "/assets/videos/winter-forest-hd_2048_1080_24fps.mp4"
+    : "/assets/videos/forest-hd_2048_1080_24fps.mp4";
 }
 
 colorButtons.forEach((button) => {
